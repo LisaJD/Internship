3369cad71a822a8ed256f41e72c606637a21c030
Lisa JD <lisa@lisajd.com>
2018-07-17 01:21:31 +0100

    Update assert.py

--- assert.py
+++ assert.py
@@ -4,11 +4,23 @@ expectation = {'client': [{150: 0}],
                 'exchange':[] }
 
 def check(expectation, observation):
-    x = expectation['client'][0]
-    y = observation['client'][0]
-    for key, value in x.items():
-        if key in y and value == y[key]:
-            continue
-        else:
-            return 0
+    if expectation['client'] != []:
+        x = expectation['client'][0]
+        y = observation['client'][0]
+        for key, value in x.items():
+            if key in y and value == y[key]:
+                continue
+            else:
+                return 0
+    if expectation['exchange'] != []:
+        a = expectation['exchange'][0]
+        b = observation['exchange'][0]
+        for key, value in a.items():
+            if key in b and value == b[key]:
+                continue
+            else:
+                return 0
     return 1
+
+
+
