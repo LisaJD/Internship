65f67e8a5a49a9bd8386c53aed292549fd6ff257
Lisa JD <lisa@lisajd.com>
2018-07-26 17:01:01 +0000

    sd
    ds

--- montecarlo/mcts.py
+++ montecarlo/mcts.py
@@ -1,18 +1,31 @@
 import random
-from engine import MainBoardCoords, SubBoardCoords, SubBoard
+from engine import MainBoardCoords, SubBoardCoords, SubBoard, MainBoard
 from players.stdout import StdOutPlayer
-#import ms.version
-#ms.version.addpkg('numpy', '1.14.2')
+import ms.version
+ms.version.addpkg('numpy', '1.14.2')
 import numpy as np
 import pdb
+import datetime
 
 
 class MonteCarlo(StdOutPlayer):
 
     def __init__(self):
         super().__init__() #means: get all initialisation attributes from base class
-        
-    def get_my_move(self):  # -> Tuple[MainBoardCoords, SubBoardCoords]
+        self.children = []
+        self.visited = True
+        self.plays = 0
+        self.wins = 0
+        self.player = 1
+        self.board_state = MainBoard(3) #should be an array storing the game state
+        self.sub_board = MonteCarlo.pick_next_main_board_coords(board_state)
+
+    def CreateChildren(self):
+        legal_moves = list(self.board_state.get_sub_board.get_playable_coords())
+        for move in legal_moves:
+            self.children.append(Node(self.board_state.add_my_move(move)))
+
+    def get_my_move(self):  # -> Tuple[MainBoardCoords, SubBoardCoords
         main_board_coords = self.pick_next_main_board_coords()
         sub_board = self.main_board.get_sub_board(main_board_coords)
         sub_board_coords = self.pick_random_sub_board_coords(sub_board)
@@ -26,45 +39,30 @@ class MonteCarlo(StdOutPlayer):
 
     @staticmethod
     def pick_random_sub_board_coords(sub_board: SubBoard) -> SubBoardCoords: #this means: argument should be of type 'SubBoard' and
-        node = Node(board_state=sub_board) #do we need to pass legal moves now so we can make the kids?)
-        node.CreateChildren()
+        # #do we need to pass legal moves now so we can make the kids?)
+        self.CreateChildren()
         print(main_board)
-        return monte_carlo_tree_search(node)
+        return monte_carlo_tree_search(self)
 
-import datetime
-
-class Node(object):
-    def __init__(self, board_state):
-        self.children = []
-        self.visited = True
-        self.plays = 0
-        self.wins = 0
-        self.player = 1
-        self.board_state = SubBoard(3) #should be an array storing the game state
-
-    def CreateChildren(self):
-        legal_moves = list(self.board_state.get_playable_coords())
-        for move in legal_moves:
-            self.children.append(Node(self.board_state.add_my_move(move)))
-
-def monte_carlo_tree_search(node):
-    begin = datetime.datetime.utcnow()
+    def monte_carlo_tree_search(node):
+        begin = datetime.datetime.utcnow()
     while datetime.datetime.utcnow() - begin < datetime.timedelta(milliseconds=4.9):
         #if all root node's children are visited:
         if all(child.visited for child in node.children):
             for child in node.children:
                 print(child.visited)
-        #next node to explore is one with max UCT
+                #next node to explore is one with max UCT
             return max_uct(node)   #this returns the best poss next move
         #if some unvisited:
         else:
             node.run_simulation(node)
     return max_uct(node)
 
-def run_simulation(node):
-    pdb.set_trace()
-    print('mainboard:', MainBoard)
-    if MainBoard.is_finished: ###
+
+    def run_simulation(node):
+        pdb.set_trace()
+    print('mainboard:' + self.main_board)
+    if self.MainBoard.is_finished(): ###
         node.plays += 1 #shouldn't be self.?
         if MainBoard.winner == 1:
             node.wins += 1
@@ -82,13 +80,12 @@ def run_simulation(node):
         node.wins = wins + run_simulation(next_child)
         print('wins:', node.wins)
         #make this wins, play = ?
-
-def max_uct(node, c=1.4):
-    for child in node.children:
-        print('wins:', child.wins, 'plays:', child.plays)
-    uct_vals = [
-        (child.wins / (child.plays)) + c * np.sqrt((2 * np.log(node.wins) / (node.plays)))
-        for child in node.children
-    ] #first terms = ratio wins of child, second term is total ratio wins
-    return 'hello' #node.children[np.argmax(uct_vals)]
-    #np.argmax returns indices of largest value in array
+    def max_uct(node, c=1.4):
+        for child in node.children:
+            print('wins:', child.wins, 'plays:', child.plays)
+        uct_vals = [
+            (child.wins / (child.plays)) + c * np.sqrt((2 * np.log(node.wins) / (node.plays)))
+            for child in node.children
+            ] #first terms = ratio wins of child, second term is total ratio wins
+        return 'hello' #node.children[np.argmax(uct_vals)]
+        #np.argmax returns indices of largest value in array
